7995cee3629e2409b70772900197f45cbdb2ae15
shaumiredmi-ai <shaumiredmi@gmail.com>
2026-03-02 09:14:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,30 @@ const qrcode = require("qrcode-terminal");
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ["--no-sandbox", "--disable-setuid-sandbox"]
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-accelerated-2d-canvas",
+            "--no-first-run",
+            "--no-zygote",
+            "--disable-gpu"
+        ]
     }
 });
 
 client.on("qr", (qr) => {
-    console.log("Scan QR ini:");
+    console.log("=========== QR CODE ===========");
     qrcode.generate(qr, { small: true });
 });
 
 client.on("ready", () => {
-    console.log("Bot siap!");
+    console.log("WhatsApp BOT SIAP");
+});
+
+client.on("disconnected", () => {
+    console.log("Bot disconnected");
 });
 
 client.initialize();
